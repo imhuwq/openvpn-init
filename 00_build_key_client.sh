17cfe5e7050b59ee67910903215a03f5e7895fcb
imhuwq <imhuwq@gmail.com>
2019-06-29 01:06:26 +0800

    new(client_scripts): add scripts to create client config

--- 00_build_key_client.sh
+++ 00_build_key_client.sh
@@ -1,4 +1,39 @@
 cd openvpn-ca
 source ./vars
+export KEY_NAME="${1}"
 
-./build-key $1
+./build-key $1 << EOF
+
+
+
+
+
+
+
+
+
+
+y
+y
+EOF
+
+cd ..
+KEY_DIR=./openvpn-ca/keys
+OUTPUT_DIR=./client-cfg/clients
+BASE_CONFIG=./client-cfg/base.conf
+
+cat ${BASE_CONFIG} \
+    <(echo -e '<ca>') \
+    ${KEY_DIR}/ca.crt \
+    <(echo -e '</ca>\n<cert>') \
+    ${KEY_DIR}/${1}.crt \
+    <(echo -e '</cert>\n<key>') \
+    ${KEY_DIR}/${1}.key \
+    <(echo -e '</key>\n<tls-auth>') \
+    ${KEY_DIR}/ta.key \
+    <(echo -e '</tls-auth>') \
+    > ${OUTPUT_DIR}/${1}.ovpn
+
+echo ""
+echo "${OUTPUT_DIR}/${1}.ovpn"
+echo ""
---
+++ 07_prepare_client.sh
@@ -0,0 +1,22 @@
+mkdir -p client-cfg/clients
+chmod 700 client-cfg/clients
+cp /usr/share/doc/openvpn/examples/sample-config-files/client.conf client-cfg/base.conf
+cd client-cfg
+
+sed -i "s/remote my-server-1 1194/remote ${1} 22222/g" base.conf
+sed -i "s/;user nobody/user nobody/g" base.conf
+sed -i "s/;group nogroup/group nogroup/g" base.conf
+
+sed -i "s/^ca ca.crt/# ca ca.crt/g" base.conf
+sed -i "s/^cert client.crt/# cert client.crt/g" base.conf
+sed -i "s/^key client.key/# key client.key/g" base.conf
+
+sed -i "/;cipher x/a key-direction 1" base.conf
+sed -i "/;cipher x/a auth SHA256" base.conf
+sed -i "s/;cipher x/cipher AES-128-CBC/g" base.conf
+
+echo "" >> base.conf
+echo "# uncomment following three lines for linux clients" >> base.conf
+echo "# script-security 2" >> base.conf
+echo "# up /etc/openvpn/update-resolv-conf" >> base.conf
+echo "# down /etc/openvpn/update-resolv-conf" >> base.conf
